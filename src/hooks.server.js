0b2856808b700c53bdb93a0c1845c87e0d4f8cfb
jorj-kare <jorj.kare@gmail.com>
2023-03-09 14:33:34 +0100

    feat: add hooks for the server

---
+++ src/hooks.server.js
@@ -0,0 +1,37 @@
+import User from '$lib/server/db/models/user';
+import { connectToDB } from '$lib/server/db/mongoDB';
+import { verifyToken } from '$lib/server/utils/jsonwebtoken';
+
+import { MODE } from '$env/static/private';
+
+connectToDB().then(async () => {
+	console.log('You have successfully connected to the database!');
+});
+
+export const handle = async ({ event, resolve }) => {
+	const jwt = event.cookies.get('jwt');
+
+	if (jwt) {
+		const decoded = await verifyToken(jwt);
+		const user = await User.findById(decoded?.id);
+		if (user) {
+			const { username, id, profileImg } = user;
+			event.locals.user = {
+				user: {
+					username,
+					id,
+					profileImg
+				}
+			};
+		}
+	}
+
+	return await resolve(event);
+};
+
+export const handleError = ({ error }) => {
+	if (MODE === 'development') console.log(error);
+	return {
+		message: 'An unexpected error has occurred'
+	};
+};
