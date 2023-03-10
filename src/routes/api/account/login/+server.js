460c9dd6af0f3d2049d3ca36b3890e62ff1be669
jorj-kare <jorj.kare@gmail.com>
2023-03-10 14:04:56 +0100

    fix(api-login): error handling

--- src/routes/api/account/login/+server.js
+++ src/routes/api/account/login/+server.js
@@ -1,38 +1,32 @@
 import User from '$lib/server/db/models/user';
 import { error, json } from '@sveltejs/kit';
 import { signToken } from '$lib/server/utils/jsonwebtoken';
-import { checkError } from '$lib/server/utils/customErrors';
 
 export const POST = async ({ cookies, request }) => {
-	try {
-		const formData = await request.formData();
-		const username = formData.get('username');
-		const password = formData.get('password')?.toString();
-		if (!username || !password) {
-			throw error(404, 'Please provide username and password');
-		}
+	const formData = await request.formData();
+	const username = formData.get('username');
+	const password = formData.get('password')?.toString();
 
-		const user = await User.findOne({ username }).select('+password');
-
-		if (!user || !(await user?.isPasswordCorrect(password, user.password))) {
-			throw error(404, 'Incorrect username or password');
-		}
-		user.password = '';
-
-		const token = signToken(user._id.toHexString());
-
-		cookies.set('jwt', token, {
-			path: '/'
-		});
-		return json({
-			token,
-			data: {
-				user
-			}
-		});
-	} catch (err) {
-		const { msg, statusCode } = checkError(err);
-
-		throw error(statusCode, msg);
+	if (!username || !password) {
+		throw error(404, 'Please provide username and password');
 	}
+
+	const user = await User.findOne({ username }).select('+password');
+
+	if (!user || !(await user?.isPasswordCorrect(password, user.password))) {
+		throw error(404, 'Incorrect username or password');
+	}
+	user.password = '';
+
+	const token = signToken(user._id.toHexString());
+
+	cookies.set('jwt', token, {
+		path: '/'
+	});
+	return json({
+		token,
+		data: {
+			user
+		}
+	});
 };
