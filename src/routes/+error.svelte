728bd20c0dc8501aafb76b778ae6cd406e917de3
jorj-kare <jorj.kare@gmail.com>
2023-03-27 12:12:31 +0200

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+	import getRandomColor from '$lib/utils/getRandomColor.js';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	onMount(() => {
+		setTimeout(() => {
+			goto('/');
+		}, 1500);
+	});
+</script>
+
+<div style:background-color={getRandomColor()} class="wrapper">
+	<h1>{$page.status}</h1>
+	<h2>{$page.error?.message}</h2>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 5rem;
+		min-height: 100vh;
+		color: #eee;
+	}
+	h1 {
+		font-size: 15rem;
+	}
+	h2 {
+		font-size: 5rem;
+	}
+</style>
