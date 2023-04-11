a7fac4912381cf54904480a435d8f863bb7fcbab
jorj-kare <jorj.kare@gmail.com>
2023-04-11 17:53:39 +0200

    fix: text on notifications

--- src/lib/components/form/validations.js
+++ src/lib/components/form/validations.js
@@ -1,8 +1,7 @@
 export function isPasswordsTheSame(password, confirmPassword) {
-	if (password && password !== confirmPassword)
-		return 'Οι κωδικοί δεν είναι ίδιοι';
+	if (password && password !== confirmPassword) return 'Οι κωδικοί δεν είναι ίδιοι';
 }
 
 export function isFieldEmpty(value, label = 'Πεδίο') {
-	if (!value) return `${label} είναι υποχρεωτικό`;
+	if (!value) return `${label}: το πεδίο είναι υποχρεωτικό`;
 }
